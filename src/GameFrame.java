24dbd25920a225ec2440b9a8642e2e35800acdaf
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-06-28 20:19:40 +0530

    game frame class

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -4,5 +4,12 @@ public class GameFrame extends JFrame {
 
 	GameFrame(){
 		
+		this.add(new GamePanel());
+		this.setTitle("Snake"); // title of the game
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exits the game on closing the window
+		this.setResizable(false); // can't resize the window
+		this.pack(); //automatically size the frame based on the contained components.
+		this.setVisible(true); // makes frame visible
+		this.setLocationRelativeTo(null); // makes frame display in center of the screen
 	}
 }
